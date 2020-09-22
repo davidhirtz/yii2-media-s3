06b60ff9403038064547056f04e50b8ae8e54836
davidhirtz <david.hirtz@me.com>
2020-09-22 14:59:24 +0200

    Bucket name

--- composer/Bootstrap.php
+++ composer/Bootstrap.php
@@ -1,15 +1,16 @@
 <?php
 
-namespace davidhirtz\yii2\media\composer;
+namespace davidhirtz\yii2\media\s3\composer;
 
 use davidhirtz\yii2\skeleton\composer\BootstrapTrait;
 use yii\base\Application;
 use yii\base\BootstrapInterface;
 use Yii;
+use yii\base\InvalidConfigException;
 
 /**
  * Class Bootstrap
- * @package davidhirtz\yii2\media\bootstrap
+ * @package davidhirtz\yii2\media\s3\bootstrap
  */
 class Bootstrap implements BootstrapInterface
 {
@@ -20,12 +21,17 @@ class Bootstrap implements BootstrapInterface
      */
     public function bootstrap($app)
     {
-//        $this->extendModules($app, [
-//            'media' => [
-//                'class' => 'davidhirtz\yii2\media\Module',
-//                'uploadPath' => 'uploads'
-//            ],
-//        ]);
-        Yii::debug('S3 Bootstrap');
+        if(!$bucket = ($app->getModules()['media-s3']['bucket'] ?? false)) {
+            throw new InvalidConfigException("Please specify an S3 bucket name.");
+        }
+
+        $this->extendModules($app, [
+            'media' => [
+                'uploadPath' => "s3://{$bucket}/",
+            ],
+            'media-s3' => [
+                'class' => 'davidhirtz\yii2\media\s3\Module',
+            ],
+        ]);
     }
 }
